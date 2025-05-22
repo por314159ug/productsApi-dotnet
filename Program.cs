e42c3c757fd8a4502850e102c36344df50025efd
por314159ug <por.314.u.g@gmail.com>
2025-05-22 11:28:23 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,19 +3,41 @@ using ProductsApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
+// Konfiguracja SqlServer zamiast bazy w pamięci
 builder.Services.AddDbContext<ProductContext>(options =>
-    options.UseInMemoryDatabase("Products"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductContext")));
+
+// Konfiguracja CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+// Użycie CORS
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
